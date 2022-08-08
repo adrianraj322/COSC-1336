a07fa236b91cc6a4b0bcd5e23abcbdc1cdbea5d5
adrianraj322 <adrianraj322@gmail.com>
2022-08-08 18:39:51 -0500

    Create Practice_2-2.py

---
+++ Practice_2-2.py
@@ -0,0 +1,11 @@
+#Adrian Raj, Status-Complete
+#Find out your net monthly income, yearly gross pay, and yearly net pay.
+monthly_gross_income = 3500
+monthly_payment_deductions = 900
+net_monthly_income = monthly_gross_income - monthly_payment_deductions
+yearly_gross_pay = monthly_gross_income * 12
+yearly_net_pay = net_monthly_income * 12
+
+print('Your net monthly income is', net_monthly_income)
+print('Your yearly gross pay is', yearly_gross_pay)
+print('Your yearly net pay is', yearly_net_pay)
