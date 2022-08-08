b638c11007a0540b9bb68ee0d56e0c1c85a1fa4f
adrianraj322 <adrianraj322@gmail.com>
2022-08-08 18:38:15 -0500

    Create Practice_2-1.py

---
+++ Practice_2-1.py
@@ -0,0 +1,11 @@
+#Adrian Raj, Status-Complete
+#Add and find the average of the cost of a hamburger, fries, and shake.
+
+hamburger_cost = 5
+fries_cost = 3
+shake_cost = 1
+total_cost = hamburger_cost + fries_cost + shake_cost
+average_cost = total_cost / 3
+
+print('Your total cost is', total_cost)
+print('Your average cost is', average_cost)
